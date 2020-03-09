0d82fc37061c6f8c557af1efbb7a010ed3479df0
juanjo <31669195+GrupoWeb@users.noreply.github.com>
2020-03-09 16:16:59 -0600

    dash

---
+++ laravel.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/GrupoWeb/karrillo
+cd karrillo
+cd laradock
+cd env.example .env
+sudo docker-compose up -d nginx
+sudo docker-compose exec workspace bash
+composer install
+npm install
+cp .env.example .env
+artisan key:generate
+exit
+cd ..
+cd karrillo_dash
+sudo chmod -R 777 storage bootstrap/cache
--- script-shell.sh
+++ script-shell.sh
@@ -7,3 +7,4 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 sudo apt-get update && apt-get install docker-ce
 curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
